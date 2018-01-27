06fd96b45c46d743fc1d87012ae950ae8721846c
Aileen <aileen.w.zeng@gmail.com>
2018-01-27 14:44:15 -0500

    pushing working code sorry

--- scripts/coffee.sh
+++ scripts/coffee.sh
@@ -2,10 +2,14 @@
 array=("hello" "123" "goodbye")
 
 echo --- Executing coffee shell script ---
+
+testString="$(java HelloWorld)"
+echo $testString
+
 for count in {0..2}
 do
     echo Iteration $count    #prints out counter for array
-    echo "${array[$count]}" | java ScannerTest    #pipes input from shell into java file
+    echo "${array[$count]}" | java ScannerTest > output.txt    #pipes input from shell into java file
     echo 
 done
 echo --- Terminating coffee shell script ---
